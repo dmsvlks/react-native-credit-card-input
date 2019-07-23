64b8ec875893047c7fee4f48d432fcfda2be4fe0
Domas <domas.velykis@gmail.com>
2019-07-23 13:52:06 +0300

    change card image based on type

---
+++ src/CardImages.js
@@ -0,0 +1,10 @@
+const Icons = {
+  imageFrontDefault: require('../images/card-front.png'),
+  imageFrontVisa: require('../images/card-front-visa.png'),
+  imageFrontMastercard: require('../images/card-front-mastercard.png'),
+  imageBackDefault: require('../images/card-back.png'),
+  imageBackVisa: require('../images/card-back-visa.png'),
+  imageBackMastercard: require('../images/card-back-mastercard.png')
+};
+
+export default Icons;
